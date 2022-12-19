e7f631134adc8728eb8a4244975fe90841a63288
aavidan <amir.avidan@beyondminds.ai>
2022-12-19 14:37:11 +0200

    Only getting info from sensor is missing

--- amir.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python3 setValueBySensor.py temperature 200
-python3 shell.py << EOF
-RUN("examp.txt")
-EOF
--- shell.py
+++ shell.py
@@ -1,14 +1,13 @@
 import basic
+import sys
 
-while True:
-	text = input('SmartByte Lang > ')
-	if text.strip() == "": continue
-	result, error = basic.run('<stdin>', text)
+if sys.argv[1]:
+    result, error = basic.run('<stdin>', sys.argv[1])
 
-	if error:
-		print(error.as_string())
-	elif result:
-		if len(result.elements) == 1:
-			print(repr(result.elements[0]))
-		else:
-			print(repr(result))
+if error:
+    print(error.as_string())
+elif result:
+    if len(result.elements) == 1:
+        print(repr(result.elements[0]))
+    else:
+        print(repr(result))
